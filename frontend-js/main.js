d43e537e66a662a72433d943b2c9e8ecf190bca4
George <g.peck.iii@gmail.com>
2023-04-22 18:04:10 -0700

    Commit New Project

---
+++ frontend-js/main.js
@@ -0,0 +1,10 @@
+import RegistrationForm from './modules/registrationForm'
+import Search from './modules/search'
+import Chat from './modules/chat'
+if(document.querySelector("#registration-form")){
+    new RegistrationForm()
+}
+if (document.querySelector(".header-search-icon")) {new Search()}
+if(document.querySelector("#chat-wrapper")){
+    new Chat()
+}
